eb78f5bcb2fb15dbbf4ac27a0d7d40d335f3fb79
Santiagodesu1 <94951105+Santiagodesu1@users.noreply.github.com>
2021-11-24 00:52:28 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,56 @@
+let Dado = 0
+input.onButtonPressed(Button.A, function () {
+    Dado = randint(1, 6)
+    if (1 == Dado) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (2 == Dado) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (3 == Dado) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (4 == Dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (5 == Dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (6 == Dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
